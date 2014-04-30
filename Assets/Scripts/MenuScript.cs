6a08f11ecf4834fbcc9ee63943675263abb44fec
Wildine Anthony <anthon.wild@gmail.comgit config --global user.email anthon.wild@gmail.comgit config --global user.email anthon.wild@gmail.com>
2014-04-30 17:45:27 +0200

    Start

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+	private GUISkin skin;
+
+	void Start()
+	{
+		skin = Resources.Load("ButtonGUI") as GUISkin;
+	}
+
+	void OnGUI()
+	{
+		const int buttonWidth = 94;
+		const int buttonHeight = 60;
+
+		GUI.skin = skin;
+
+		if (
+			GUI.Button(
+			// Center in X, 2/3 of the height in Y
+			new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(2 * Screen.height / 3) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			),
+			"Start !"
+			)
+			)
+		{
+			Application.LoadLevel("start");
+		}
+	}
+}
