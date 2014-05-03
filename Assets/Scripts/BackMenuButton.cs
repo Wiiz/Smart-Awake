bb29558bc11785364bac3bcc7d94252a3b9c37b0
Wildine Anthony <anthon.wild@gmail.comgit config --global user.email anthon.wild@gmail.comgit config --global user.email anthon.wild@gmail.com>
2014-05-03 19:33:18 +0200

    score

---
+++ Assets/Scripts/BackMenuButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackMenuButton : MonoBehaviour {
+
+	private GUISkin skin;
+	
+	void Start()
+	{
+		skin = Resources.Load("ButtonGUI") as GUISkin;
+	}
+	
+	void OnGUI()
+	{
+		const int buttonWidth = 120;
+		const int buttonHeight = 60;
+		
+		GUI.skin = skin;
+		
+		if (GUI.Button(
+			new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(4 * Screen.height / 5) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			),
+			"Menu"
+			)
+		    )
+		{
+			Application.LoadLevel("Menu");
+		}
+	}
+}
