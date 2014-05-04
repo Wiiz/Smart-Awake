f880a56cfefe1db1875d82f80875126a19539af0
ASKz <saddok_m@etna-alternance.net>
2014-05-04 18:22:13 +0200

    effects, and scripts

--- Assets/Scripts/CheckLetters.cs
+++ Assets/Scripts/CheckLetters.cs
@@ -5,47 +5,67 @@ using System;
 
 public class CheckLetters : MonoBehaviour
 {
+
+	bool isHit;
+	GameObject currentGo;
+	
 	void OnGUI () 
 	{ 
 		GUI.depth = 2;
-		bool isHit = false;
+		isHit = false;
+	}
+
+	void Update()
+	{
 		Ray rayMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hitMouse;
 		int missed = 0;
 		char currentLetter = '\0';
+
 		if(Input.GetMouseButtonDown(0))
 		{
-			if(Physics.Raycast(rayMouse,out hitMouse))
+			if(Physics.Raycast(rayMouse, out hitMouse) && !isHit)
 			{
 				currentLetter = onHit (hitMouse);
-
-
+				isHit = true;
+				
 			} else {
 				missed++;
+				isHit = false;
 			}
+			isHit = false;
 		}
-		SendMessage("renderLetter" , currentLetter);
 	}
 
+	void destructCurrentGo()
+	{
+		Destroy(currentGo);
+	}
 
 	char onHit (RaycastHit hit)
 	{
-		GameObject current = hit.collider.gameObject;
-		char currentLetter = rayLetter (current);
+		if (isHit) 
+		{
+			currentGo = hit.collider.gameObject;
+			char currentLetter = rayLetter (currentGo);
 
-
-
-		Destroy(current);
-		SoundEffect.Instance.MakeBoopSound();
-		CloudEffect.Instance.Explosion(hit.point);
-
-		return(currentLetter);
+			SoundEffect.Instance.MakeBoopSound (); //making some noise
+			CloudEffect.Instance.Explosion (hit.point); //making xploding
+			Invoke("destructCurrentGo", 1);
+			 //destroy object
+			return(currentLetter);
+		} 
+		else 
+		{
+			return '0';                                                        
+		}
 	}
 
 
 
 	char rayLetter (GameObject go)
 	{
+		// name of clouds = CloudX, CloudA, etc.. 
 		return (go.name[5]);
 	}
 
--- Assets/Scripts/FloatScript.js
+++ Assets/Scripts/FloatScript.js
@@ -2,15 +2,34 @@
  
 var floatSpan = 2.0;
 var speed  = 1.0;
+var strayFactor : int;
+var rdm : int;
+var range : int;
 
 private var startY : float;
+private var startX : float;
 
 function Start() 
 {
     startY = transform.position.y;
+    startX = transform.position.x;
+    rdm = 0;
 }
 
 function Update()
 {
-    transform.position.y = startY + Mathf.Sin(Time.time * speed) * floatSpan / 2.0;
+
+	var randomX = Random.Range(-strayFactor, strayFactor);
+	var randomY = Random.Range(-strayFactor, strayFactor);
+	var randomZ = Random.Range(-strayFactor, strayFactor);
+  
+    transform.position.y = strayFactor + (startY * Mathf.Sin(Time.time * speed));
+    
+    transform.position.x = strayFactor + (startX * Mathf.Sin(Time.time * speed));
+}
+
+
+function makeFloat()
+{
+  transform.position.y = startY + Mathf.Sin(Time.time * speed) * floatSpan / 2.0;
 }
